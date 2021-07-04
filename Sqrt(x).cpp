d97e425aa5c6caa28210aa704657858da3e240db
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-05 00:37:55 +0530

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int mySqrt(int x) {
+    //     int sq=x/2;
+    //     int temp=0;
+    //     while(sq!=temp)
+    //     {
+    //         temp=sq;
+    //         sq=(x/temp+temp)/2;
+    //     }
+    //     return sq;
+        return sqrt(x);
+    }
+};
