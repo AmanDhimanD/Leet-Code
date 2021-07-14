762b7c3c34f028b36dd1e00e8506ba3908000a73
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-07-14 23:44:53 +0530

    Update Sum of two Integers.cpp

--- Sum of two Integers.cpp
+++ Sum of two Integers.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int c;
-        while(b!=0)
-        {
-            c=a&b;
-            a=a^b;
-            b=c<<1;
-        }
-        return a;
+      return log(exp(a)*exp(b));
     }
 };
