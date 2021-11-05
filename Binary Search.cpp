5f337ce93878221a202c0ef823978b8a93c6c0d8
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-11-05 11:14:24 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
+        int s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-          
+            else if(target<nums[mid]){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
         return -1;
     }
-};
+};  
+// 50% Run 
+//         for(int i =0;i<nums.size();i++){
+//             if(nums[i]==target){
+//                 return i;
+//             }
+          
+//         }
+//         return -1;
