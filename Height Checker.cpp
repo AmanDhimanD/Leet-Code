9ae1c26d98d16d3122c23b46e272734c1b987bef
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-12-03 21:52:14 +0530

    Create Height Checker.cpp

---
+++ Height Checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> v;
+        v= heights;
+        int c=0;
+        sort(v.begin(),v.end());
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=v[i]){
+                c++;
+            }
+        }
+        return c;
+    }
+};
