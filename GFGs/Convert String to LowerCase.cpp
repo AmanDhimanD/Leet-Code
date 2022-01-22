497db4e13ac3b187552a68948d8f3da5b529b4c9
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-22 14:20:13 +0530

    Update Convert String to LowerCase.cpp

--- GFGs/Convert String to LowerCase.cpp
+++ GFGs/Convert String to LowerCase.cpp
@@ -2,7 +2,15 @@ class Solution {
   public:
     string toLower(string S) {
         // code here
-        transform(S.begin(), S.end(), S.begin(), ::tolower);
+//         transform(S.begin(), S.end(), S.begin(), ::tolower);
+//         return S;
+      
+      int n=S.length();
+        for(int i=0;i<n;i++){
+            if(S[i]>='A' && S[i]<='Z'){
+                S[i]=S[i]^32;
+            }
+        }
         return S;
     }
 };
