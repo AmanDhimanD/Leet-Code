94b3321e487177f5604b757c8534bdd72ee595f8
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-22 20:17:16 +0530

    Update Reverse a String.cpp

--- GFGs/Reverse a String.cpp
+++ GFGs/Reverse a String.cpp
@@ -1,7 +1,11 @@
 class Solution {
   public:
     string revStr(string S) {
-        reverse(S.begin(),S.end());
-        return S;
+        //reverse(S.begin(),S.end());
+        //return S;
+         int n=S.length();
+        for(int i=n-1;i>=0;i--){
+            cout<<S[i];
+        }
     }
 };
