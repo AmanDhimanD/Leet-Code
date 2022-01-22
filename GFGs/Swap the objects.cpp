a9e4e19cee1973cd5a020cbc03294d880f8158cc
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-22 13:01:33 +0530

    Update Swap the objects.cpp

--- GFGs/Swap the objects.cpp
+++ GFGs/Swap the objects.cpp
@@ -1,6 +1,8 @@
 void swapper(Person **obj1, Person **obj2)
 {
     //Your code here
-    swap(*obj1,*obj2);
-   
+    //swap(*obj1,*obj2);
+      Person *obj3 = *obj1;
+    *obj1 = *obj2;
+    *obj2 = obj3;
 }
