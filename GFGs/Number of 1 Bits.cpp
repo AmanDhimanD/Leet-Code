8b48bc95696efd57030e6a197fc039635133daf0
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-22 19:19:48 +0530

    Update Number of 1 Bits.cpp

--- GFGs/Number of 1 Bits.cpp
+++ GFGs/Number of 1 Bits.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int setBits(int n) {
         // Write Your Code here
-        // return __builtin_popcount(N); //This is C++ BULIT Function of GCC Complier
+        // return __builtin_popcount(N); //This is C++ BULIT Function of GCC Compiler
     unsigned int count = 0;
     while (n) {
         count += n & 1;
