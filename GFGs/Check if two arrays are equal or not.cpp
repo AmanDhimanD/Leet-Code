832ed6f35861b7bea53d11d861fe95d3e835cdf4
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-23 20:22:49 +0530

    Create Check if two arrays are equal or not.cpp

---
+++ GFGs/Check if two arrays are equal or not.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        map<ll,ll>mp;
+        for(int i=0;i<N;i++){
+            mp[A[i]]++;
+            mp[B[i]]--;
+        }
+        for(auto x:mp){
+            if(x.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
