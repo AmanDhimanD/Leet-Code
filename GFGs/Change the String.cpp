bf94c53a62e9561f57720f93144e39579b267d26
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-01-30 02:21:02 +0530

    Update Change the String.cpp

--- GFGs/Change the String.cpp
+++ GFGs/Change the String.cpp
@@ -4,12 +4,26 @@ class Solution{
     string modify (string s)
     {
         
-            if(s[0]>='A' && s[0]<='Z'){
-                transform(s.begin(), s.end(), s.begin(), ::toupper);
-            }
-            if(s[0]>='a' && s[0]<='z'){
-                transform(s.begin(), s.end(), s.begin(), ::tolower);
-            }
+//             if(s[0]>='A' && s[0]<='Z'){
+//                 transform(s.begin(), s.end(), s.begin(), ::toupper);
+//             }
+//             if(s[0]>='a' && s[0]<='z'){
+//                 transform(s.begin(), s.end(), s.begin(), ::tolower);
+//             }
+         if(s[0]>='a' && s[0]<='z'){
+             for(int i=0;i<s.size();i++){
+                 if(s[i]>='A' && s[i]<='Z'){
+                     s[i]+=32;
+                 }
+             }
+         }
+        else{
+               for(int i=0;i<s.size();i++){
+                 if(s[i]>='a' && s[i]<='z'){
+                     s[i]-=32;
+                 }
+             }  
+        }
         return s;
     }
 };
