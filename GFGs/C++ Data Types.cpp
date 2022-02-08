df857cc4dca7f703797a33ce5e6707bddbfa84d7
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-02-08 21:52:42 +0530

    Create C++ Data Types.cpp

---
+++ GFGs/C++ Data Types.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int cppIntType() {
+        // code here
+        int i;
+        cin>>i;
+        return i;
+    }
+    
+    char cppCharType() {
+        char c;
+        cin>>c;
+        return c;
+    }
+    
+    float cppFloatType() {
+        // code here
+        float f;
+        cin>>f;
+        return f;
+    }
+};
