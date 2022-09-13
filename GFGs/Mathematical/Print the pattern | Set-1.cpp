e7030ffeb6e05eaf023190ff2428c053cd44263f
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-13 21:49:08 +0530

    Create Print the pattern | Set-1.cpp

---
+++ GFGs/Mathematical/Print the pattern | Set-1.cpp
@@ -0,0 +1,13 @@
+void printPat(int n)
+{
+//Your code here
+for(int i=0;i<n;i++){
+    for(int j=n;j>0;j--){
+        for(int k=0;k<n-i;k++){
+            cout<<j<<" ";
+        }
+    }
+    cout<<"$";
+}
+
+}
