82d6c3a81066fb60996349fadd8c096f0823cf8c
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-14 13:46:12 +0530

    Update Binary number to Decimal.cpp

--- GFGs/Mathematical/Binary number to Decimal.cpp
+++ GFGs/Mathematical/Binary number to Decimal.cpp
@@ -1,11 +1,14 @@
-int binary_to_decimal(string str) {
+ int binary_to_decimal(string str) {
         // Code here.
-        string tempStr = str;
-        int decVal = 0;
-        int base = 1;
-        int len = str.length();
+        string tempStr = str; //copy the str
+        int decVal = 0; //result into decimal 
+        int base = 1; // base - 2^0 
+        int len = str.length(); //length of string
         for(int i=len-1;i>=0;i--){
+            //loop from last string 
             if(tempStr[i] == '1'){
+                //if last digit 1 then 1*2^i 
+                //add it into decimal 
                 decVal+=base;
             }
             base*=2;
