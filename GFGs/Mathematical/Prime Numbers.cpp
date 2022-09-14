c0b03d24d31f27907a7c3cd2fc2414de2d1f9d5d
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-14 22:24:27 +0530

    Create Prime Numbers.cpp

---
+++ GFGs/Mathematical/Prime Numbers.cpp
@@ -0,0 +1,15 @@
+long long int largestPrimeFactor(int N){
+        // code here
+        vector<long long int>ans;
+        for(int i=2;i<=N;i++){
+            
+            while(N%i ==0){
+                ans.push_back(i);
+                N/=i;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        
+        long long int ans1 = ans[ans.size()-1];
+        return ans1;
+    }
