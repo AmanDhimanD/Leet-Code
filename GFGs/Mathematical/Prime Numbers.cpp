345ac3b378281a133afbcd5f10a605f4e8b8981b
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-14 22:32:18 +0530

    Update Prime Numbers.cpp

--- GFGs/Mathematical/Prime Numbers.cpp
+++ GFGs/Mathematical/Prime Numbers.cpp
@@ -14,3 +14,29 @@ long long int largestPrimeFactor(int N){
         //long long int ans1 = ans.back();
         return ans.back();
     }
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    vector<int>a;
+    for(int i=2;i<=N;i++){
+        while(N%i==0){
+            cout<<"i -> "<<i<<endl;
+            a.push_back(i);
+            cout<<"N -> "<<N<<" "<<endl;
+            N/=i;
+        }
+    }
+    
+    for(auto p:a){
+        cout<<"->"<<p<<" ";
+    }
+
+    return 0;
+}
+*/
