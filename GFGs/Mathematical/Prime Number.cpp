89b0c16fb4d150efafef7c10377b6701b962c027
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-15 23:18:19 +0530

    Create Prime Number.cpp

---
+++ GFGs/Mathematical/Prime Number.cpp
@@ -0,0 +1,14 @@
+ int isPrime(int n){
+        // code here
+          if (n <= 1)
+        return false;
+ 
+    // Check from 2 to square root of n
+    for (int i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
+        
+        
+    }
