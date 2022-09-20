aa8828f686fba65fec52f0f1c6ce74da6f7f9f41
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-09-21 01:10:07 +0530

    Create Second Largest.cpp

---
+++ GFGs/Array/Second Largest.cpp
@@ -0,0 +1,13 @@
+int print2largest(int arr[], int n) {
+	    // code here
+	    
+	    sort(arr,arr+n);
+	    
+	    int lar = arr[n-1];
+	    for(int i=n-2;i>=0;i--){
+	        if(arr[i]!=lar){
+	            return arr[i];
+	        }
+	    }
+	    return -1;
+	}
